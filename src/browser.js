60ce00951b23dddd5742924d1bc6ac253bd84720
Wes Todd <wes@wesleytodd.com>
2016-02-06 18:02:28 -0600

    fixed using res.locals as the store because express uses Object.create(null) to make res.locals

--- src/browser.js
+++ src/browser.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var setPrototypeOf = require('setprototypeof');
 
 export default function reactExpressMiddlewareGenerator (options = {}) {
 	// Get the element, defaults to body
@@ -21,16 +22,22 @@ export default function reactExpressMiddlewareGenerator (options = {}) {
 			// store defaults to res.locals
 			if (typeof store === 'function') {
 				done = store;
+				store = undefined;
 			}
 			if (typeof store !== 'object') {
 				store = res.locals;
+
+				// Override prototype on res.locals because react
+				// uses res.locals.hasOwnProperty and Express specefically
+				// uses Object.create(null) which means it doesn't have hasOwnProperty
+				setPrototypeOf(store, {});
 			}
 
 			// Create factory for component
 			Component = React.createFactory(Component);
 
 			// Render template with string
-			ReactDOM.render(Component(...store), options.element, done);
+			ReactDOM.render(Component(store), options.element, done);
 		};
 		next();
 	};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOMServer = require('react-dom/server');
+var setPrototypeOf = require('setprototypeof');
 
 export default function reactExpressMiddlewareGenerator (options = {}) {
 	options.template = options.template || 'index';
@@ -10,9 +11,15 @@ export default function reactExpressMiddlewareGenerator (options = {}) {
 			// store defaults to res.locals
 			if (typeof store === 'function') {
 				done = store;
+				store = undefined;
 			}
 			if (typeof store !== 'object') {
 				store = res.locals;
+
+				// Override prototype on res.locals because react
+				// uses res.locals.hasOwnProperty and Express specefically
+				// uses Object.create(null) which means it doesn't have hasOwnProperty
+				setPrototypeOf(store, {});
 			}
 
 			// Create factory for component
@@ -20,7 +27,7 @@ export default function reactExpressMiddlewareGenerator (options = {}) {
 
 			// Render template with string
 			res.render(options.template, {
-				[options.key]: ReactDOMServer.renderToString(Component(...store))
+				[options.key]: ReactDOMServer.renderToString(Component(store))
 			}, done);
 		};
 		next();
