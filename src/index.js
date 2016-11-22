834813d34ba30ef34446fed8c16c05cc413d86cf
Amberley Romo <amberley@stream.me>
2016-11-22 11:17:33 -0600

    support option to pass in nested jsx rather than single container

--- src/index.js
+++ src/index.js
@@ -8,27 +8,39 @@ export default function reactExpressMiddlewareGenerator (options = {}) {
 
 	return function reactExpressMiddleware (req, res, next) {
 		res.renderReactComponent = function renderReactComponent (Component, store, done) {
-			// store defaults to res.locals
-			if (typeof store === 'function') {
-				done = store;
-				store = undefined;
+			//
+			// Option 1: Store passed in, Component is a function
+			//
+			if (arguments.length > 1) {
+				// store defaults to res.locals
+				if (typeof store === 'function') {
+					done = store;
+					store = undefined;
+				}
+				if (typeof store !== 'object') {
+					store = res.locals;
+
+					// Override prototype on res.locals because react
+					// uses res.locals.hasOwnProperty and Express specefically
+					// uses Object.create(null) which means it doesn't have hasOwnProperty
+					setPrototypeOf(store, {});
+				}
+
+				// Create factory for component
+				Component = React.createFactory(Component);
+
+				// Render template with string
+				res.render(options.template, {
+					[options.key]: ReactDOMServer.renderToString(Component(store))
+				}, done);
+			} else {
+				//
+				// Option 2: Store is not passed in, Component is JSX (with required props passed)
+				//
+				res.render(options.template, {
+					[options.key]: ReactDOMServer.renderToString(Component)
+				}, done);
 			}
-			if (typeof store !== 'object') {
-				store = res.locals;
-
-				// Override prototype on res.locals because react
-				// uses res.locals.hasOwnProperty and Express specefically
-				// uses Object.create(null) which means it doesn't have hasOwnProperty
-				setPrototypeOf(store, {});
-			}
-
-			// Create factory for component
-			Component = React.createFactory(Component);
-
-			// Render template with string
-			res.render(options.template, {
-				[options.key]: ReactDOMServer.renderToString(Component(store))
-			}, done);
 		};
 		next();
 	};
