26cde4bc657a1b81b4a2a3859ce1904e650047f2
Amberley Romo <amberley@stream.me>
2016-12-06 17:03:12 -0600

    Support passing jsx or component to renderReactComponent, closes #2
    commit 1ec6a84d72a7171d342064d0c398762358054868
    Merge: d786d00 675f8e0
    Author: Wes Todd <wes@wesleytodd.com>
    Date:   Tue Dec 6 17:02:36 2016 -0600
    
        Merge branch 'support-jsx' of https://github.com/amberleyromo/react-express-middleware into amberly-suport-jsx
    
    commit 675f8e0b7ddffd235c9c6abe0143ec44a7e062f7
    Author: Amberley Romo <amberley@stream.me>
    Date:   Tue Dec 6 15:06:55 2016 -0600
    
        move done call
    
    commit 10ce973f703cf4ac36d9a9f375061d0dbf90af7c
    Author: Amberley Romo <amberley@stream.me>
    Date:   Tue Dec 6 12:11:28 2016 -0600
    
        typo, assert no err in renderMiddleware
    
    commit 25b378e49cccaccbdb6d5947e490451d85c2e178
    Author: Amberley Romo <amberley@stream.me>
    Date:   Tue Dec 6 11:43:10 2016 -0600
    
        moved react transform to test and $$typeof prop check
    
    commit 18a5460eaa8d13cd56c2c4b207b1e84f8233f546
    Author: Amberley Romo <amberley@stream.me>
    Date:   Tue Dec 6 11:23:32 2016 -0600
    
        remove jsdom setup, mock test client element
    
    commit 592a43a671b18605f752775205e9960719789e95
    Author: Amberley Romo <amberley@stream.me>
    Date:   Thu Dec 1 12:20:15 2016 -0600
    
        consolidated renderMethod option
    
    commit 4dbae91d72019451a9da9363e436936b0b7e3483
    Author: Amberley Romo <amberley@stream.me>
    Date:   Thu Dec 1 09:47:25 2016 -0600
    
        tests added
    
    commit 5039ec93c83ed12ae057ec181e507cf7d7f77c56
    Author: Amberley Romo <amberley@stream.me>
    Date:   Wed Nov 23 10:16:49 2016 -0600
    
        rm dependencies
    
    commit 3cb4be86fbf44c96e2916279ab4d68d4c00da341
    Author: Amberley Romo <amberley@stream.me>
    Date:   Wed Nov 23 10:09:37 2016 -0600
    
        checks if jsx via component property
    
    commit 0015af5a7a053d66de0927852fc4d55fc280bb0b
    Author: Amberley Romo <amberley@stream.me>
    Date:   Wed Nov 23 09:05:38 2016 -0600
    
        added dependencies for testing, consolidated render calls
    
    commit e2d70e6fabb0d6864141274831ca4693c89f71e2
    Merge: 834813d 717341a
    Author: Amberley Romo <amberley@stream.me>
    Date:   Wed Nov 23 08:42:31 2016 -0600
    
        Merge remote-tracking branch 'origin/master' into support-jsx
    
    commit 834813d34ba30ef34446fed8c16c05cc413d86cf
    Author: Amberley Romo <amberley@stream.me>
    Date:   Tue Nov 22 11:17:33 2016 -0600
    
        support option to pass in nested jsx rather than single container

--- test/index.js
+++
@@ -1,9 +0,0 @@
-/* global describe, it */
-var assert = require('assert');
-var reactExpressMiddleware = require('../');
-
-describe('react-express-middleware', function () {
-	it('should ...', function () {
-		assert(reactExpressMiddleware);
-	});
-});
